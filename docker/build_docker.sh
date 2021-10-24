50ac9b3d438943681e4d65cdaebf8c266d3589ac
RichPauloo <richpauloo@gmail.com>
2021-10-24 14:36:55 -0700

    bugfix

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -7,7 +7,7 @@ docker build . -t docker.io/richpauloo/twm:prod.0.0.01
 # if [[ the last exit code $? indicates a successful 0 build, then
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push docker.io/richpauloo/dockertest:twm.0.0.01
+docker push docker.io/richpauloo/twm:prod.0.0.01
 else
 echo "Docker build failed"
 fi
