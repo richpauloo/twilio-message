7bcab06f09e46cc83e910a14ad1a388c31e7725b
RichPauloo <richpauloo@gmail.com>
2021-10-24 14:41:52 -0700

    fix shit

--- docker/install_packages.R
+++ docker/install_packages.R
@@ -1,2 +1,16 @@
-devtools::install_version("twilio", version = "0.1.0", 
-                          dependencies = "Depends", upgrade = "never")
+# installing package imports packages
+pkg_list <- c("twilio")
+
+for(i in pkg_list){
+  
+  if(!i %in% rownames(installed.packages())){
+    cat(paste0("\033[0;", 42, "m","Installing ", i,"\033[0m","\n"))
+    install.packages(pkgs = i, repos = "https://cran.rstudio.com/")
+  }
+  
+  
+  if(!i %in% rownames(installed.packages())){
+    stop(paste("Package", i, "is not available"))
+  }
+}
+
