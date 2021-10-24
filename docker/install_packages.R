d7d6e1f714fe7718851d862e23477dcfb06990cb
RichPauloo <richpauloo@gmail.com>
2021-10-24 15:44:49 -0700

    use here

--- docker/install_packages.R
+++ docker/install_packages.R
@@ -1,2 +1,4 @@
 devtools::install_version("twilio", version = "0.1.0", 
                           dependencies = "Depends", upgrade = "never")
+devtools::install_version("here", version = "1.0.1", 
+                          dependencies = "Depends", upgrade = "never")
