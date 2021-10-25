ee742a30ca324952c108819cbe038242ad7ec398
RichPauloo <richpauloo@gmail.com>
2021-10-24 17:38:08 -0700

    update deploy date

--- R/message.R
+++ R/message.R
@@ -5,13 +5,14 @@ tw_sid <- Sys.getenv("TWILIO_SID")
 tw_tok <- Sys.getenv("TWILIO_AUTH_TOKEN")
 tw_phone_number  <- Sys.getenv("TWILIO_PHONE_NUMBER")
 tw_target_number <- Sys.getenv("TARGET_PHONE_NUMBER")
-deploy_date <- "2021-10-22"
+deploy_date <- "2021-10-23"
 
 # bible verses mentioning "love" or "compassion"
 s <- read.csv(here::here("R/s.csv"))
 
 # counter: number of days since deploy
-i <- 1
+# i <- as.numeric(Sys.Date() - as.Date(deploy_date))
+i = 1
 
 # randomly sample counter if it's out of range of the data
 if(i > nrow(s)){
@@ -19,8 +20,8 @@ if(i > nrow(s)){
 }
 
 # configure auth
-Sys.setenv(TWILIO_SID   = tw_sid)
-Sys.setenv(TWILIO_TOKEN = tw_tok)
+# Sys.setenv(TWILIO_SID   = tw_sid)
+# Sys.setenv(TWILIO_TOKEN = tw_tok)
 
 # send message
 tw_send_message(from = tw_phone_number, 
